9983e21e21b4ebc26e11906d06531065499c2964
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2021-10-12 18:07:54 -0400

    Added initial spectral diversity metrics

---
+++ spectral_diversity.py
@@ -0,0 +1,187 @@
+"""
+SISTER
+Space-based Imaging Spectroscopy and Thermal PathfindER
+Author: Adam Chlus
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import argparse
+from collections import Counter
+import numpy as np
+import hytools_lite as htl
+from hytools_lite.io.envi import WriteENVI
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+def progbar(curr, total, full_progbar = 100):
+    '''Display progress bar.
+    Gist from:
+    https://gist.github.com/marzukr/3ca9e0a1b5881597ce0bcb7fb0adc549
+    Args:
+        curr (int, float): Current task level.
+        total (int, float): Task level at completion.
+        full_progbar (TYPE): Defaults to 100.
+    Returns:
+        None.
+    '''
+    frac = curr/total
+    filled_progbar = round(frac*full_progbar)
+    print('\r', '#'*filled_progbar + '-'*(full_progbar-filled_progbar), '[{:>7.2%}]'.format(frac), end='')
+
+def main():
+    '''
+    Spectral diversity metrics
+
+    Adapted from https://github.com/jbferet/biodivMapR
+
+    '''
+
+    desc = "Generate spectral diversity metrics"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('rfl_file', type=str,
+                        help='Input reflectance image')
+    parser.add_argument('out_dir', type=str,
+                        help='Output directory')
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--clusters', type=int, default=25,
+                        help='Export PCA image')
+    parser.add_argument('--window', type=int, default=5,
+                        help='Window size for calculating diversity metrics')
+    parser.add_argument('--pca', action='store_true',
+                        help='Export PCA image')
+    parser.add_argument('--species', action='store_true',
+                        help='Export spectral species map')
+
+    args = parser.parse_args()
+
+    # parser = argparse.ArgumentParser()
+    # args = parser.parse_args([])
+    # args.in_file = '/data2/prisma/rfl/PRS_20210604101721_20210604101725_0001_srtm/output/PRS_20210604101721_20210604101725_0001_rfl_prj_rfl'
+    # args.out_dir ='/data1/temp/'
+    # args.pca = True
+    # args.species = True
+    # args.window = 5
+    # args.clusters = 25
+
+    rfl = htl.HyTools()
+    rfl.read_file(args.rfl_file,'envi')
+    rfl.mask['sample'] = rfl.ndi() > .3
+
+    # Sample data
+    X  = subsample(rfl,.1)
+
+    # Center, scale and fit PCA transform
+    x_mean = X.mean(axis=0)[np.newaxis,:]
+    X -=x_mean
+    x_std = X.std(axis=0,ddof=1)[np.newaxis,:]
+    X /=x_std
+    X = X[~np.isnan(X.sum(axis=1)) & ~np.isinf(X.sum(axis=1)),:]
+
+    #Perform initial PCA fite
+    pca = PCA(n_components=20)
+    pca.fit(X)
+
+    #Refit
+    comps = np.argwhere(np.cumsum(pca.explained_variance_ratio_) > .99)[0][0] + 1
+    pca = PCA(n_components=comps)
+    pca.fit(X)
+
+    #Generate PCA transformed image
+    pca_transform = np.zeros((rfl.lines,rfl.columns,comps))
+    iterator = rfl.iterate(by = 'chunk',chunk_size = (500,500))
+
+    while not iterator.complete:
+        chunk = iterator.read_next()
+        X_chunk = chunk[:,:,~rfl.bad_bands].astype(np.float32)
+        X_chunk = X_chunk.reshape((X_chunk.shape[0]*X_chunk.shape[1],X_chunk.shape[2]))
+        X_chunk -=x_mean
+        X_chunk /=x_std
+        X_chunk[np.isnan(X_chunk) | np.isinf(X_chunk)] = 0
+        pca_chunk=  pca.transform(X_chunk)
+        pca_chunk = pca_chunk.reshape((chunk.shape[0],chunk.shape[1],comps))
+        pca_chunk[chunk[:,:,0] == rfl.no_data] =0
+        pca_transform[iterator.current_line:iterator.current_line+pca_chunk.shape[0],
+                      iterator.current_column:iterator.current_column+pca_chunk.shape[1]] = pca_chunk
+
+    #Export PCA image
+    if args.pca:
+        header = rfl.get_header()
+        header['bands']= comps
+        header['band names']= ['pca_%02d' for band in range(comps)]
+        header['wavelength']= []
+        header['fwhm']= []
+        out_file = args.out_dir + rfl.base_name + '_pca'
+        writer = WriteENVI(out_file,header)
+        for band in range(comps):
+            writer.write_band(pca_transform[:,:,band],band)
+
+    #Cluster PCA data
+    pca_sample= pca.transform(X.astype(float))
+    clusters = KMeans(n_clusters=25)
+    clusters.fit(pca_sample)
+    classes = clusters.predict(pca_transform.reshape(rfl.lines*rfl.columns,comps)).reshape(rfl.lines,rfl.columns)
+
+    if args.species:
+        header = rfl.get_header()
+        header['bands']= 1
+        header['band names']= ['species']
+        header['wavelength']= []
+        header['fwhm']= []
+        classes[rfl.ndi() < .3] = rfl.no_data
+        out_file = args.out_dir + rfl.base_name + '_species'
+        writer = WriteENVI(out_file,header)
+        writer.write_band(classes,0)
+
+    shannon = np.zeros((rfl.lines,rfl.columns))
+    simpson = np.zeros((rfl.lines,rfl.columns))
+
+    for line in range(args.window,rfl.lines-args.window):
+        for col in range(args.window,rfl.columns-args.window):
+            neighborhood = classes[line-args.window:line+args.window,col-args.window:col+args.window]
+            neighborhood = neighborhood[neighborhood!=rfl.no_data]
+
+            neigh_sum= np.array(list(Counter(neighborhood.flatten()).values()))
+
+            sh = -np.sum(neigh_sum/neigh_sum.sum() * np.log(neigh_sum/neigh_sum.sum()))
+            sim = 1- ((neigh_sum*(neigh_sum-1)).sum())/(neigh_sum.sum()*(neigh_sum.sum()-1))
+
+            shannon[line,col] = sh
+            simpson[line,col] = sim
+        if args.verbose:
+            progbar(line,rfl.lines, full_progbar = 100)
+
+    #Export diversity indices
+    out_file = args.out_dir + rfl.base_name + '_diversity'
+    header = rfl.get_header()
+    header['bands']= 2
+    header['band names']= ['shannon','simpson']
+    writer = WriteENVI(out_file,header)
+    writer.write_band(shannon[:,:],0)
+    writer.write_band(simpson[:,:],1)
+
+def subsample(hy_obj,sample_size):
+
+    sub_samples = np.zeros((hy_obj.lines,hy_obj.columns)).astype(bool)
+    idx = np.array(np.where(hy_obj.mask['sample'])).T
+    idxRand= idx[np.random.choice(range(len(idx)),int(len(idx)*sample_size), replace = False)].T
+    sub_samples[idxRand[0],idxRand[1]] = True
+    hy_obj.mask['samples'] = sub_samples
+
+    X = []
+
+    hy_obj.create_bad_bands([[300,400],[1300,1450],[1780,2000],[2450,2600]])
+    for band_num,band in enumerate(hy_obj.bad_bands):
+        if ~band:
+            X.append(hy_obj.get_band(band_num,mask='samples'))
+    return  np.array(X).T
+
+if __name__== "__main__":
+    main()
