22d5fcf87020ae7e9b40b299aadca2eb939cee8c
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2021-10-15 17:37:36 -0400

    Added beta diversity calculator

--- spectral_diversity.py
+++ spectral_diversity.py
@@ -14,14 +14,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import argparse
-from collections import Counter
 import numpy as np
 import hytools_lite as htl
 from hytools_lite.io.envi import WriteENVI
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.manifold import MDS
-
+from skimage.util import view_as_blocks
+from numba import jit
 
 def progbar(curr, total, full_progbar = 100):
     '''Display progress bar.
@@ -38,11 +38,112 @@ def progbar(curr, total, full_progbar = 100):
     filled_progbar = round(frac*full_progbar)
     print('\r', '#'*filled_progbar + '-'*(full_progbar-filled_progbar), '[{:>7.2%}]'.format(frac), end='')
 
+def subsample(hy_obj,sample_size):
+    sub_samples = np.zeros((hy_obj.lines,hy_obj.columns)).astype(bool)
+    idx = np.array(np.where(hy_obj.mask['sample'])).T
+    idxRand= idx[np.random.choice(range(len(idx)),int(len(idx)*sample_size), replace = False)].T
+    sub_samples[idxRand[0],idxRand[1]] = True
+    hy_obj.mask['samples'] = sub_samples
+
+    X = []
+
+    hy_obj.create_bad_bands([[300,400],[1300,1450],[1780,2000],[2450,2600]])
+    for band_num,band in enumerate(hy_obj.bad_bands):
+        if ~band:
+            X.append(hy_obj.get_band(band_num,mask='samples'))
+    return  np.array(X).T
+
+@jit(nopython=True)
+def calc_bray_curtis_blocks(image_blocks,nclusters):
+
+    windows_pixels = image_blocks.shape[1]*image_blocks.shape[2]
+    bray_curtis = np.full((image_blocks.shape[0],image_blocks.shape[0]),-4.0)
+
+    i=0
+    for nbhd1 in image_blocks:
+        nbhd1 = nbhd1.flatten()
+        cover1 = [0 for x in range(nclusters)]
+        nbhd1_size = 0
+
+        for a in nbhd1:
+            if a !=nclusters:
+                cover1[a]+=1
+                nbhd1_size+=1
+        if nbhd1_size/windows_pixels < .75:
+            i+=1
+            continue
+        j=i
+        for nbhd2 in image_blocks[i:]:
+            nbhd2 = nbhd2.flatten()
+            cover2 = [0 for x in range(nclusters)]
+            nbhd2_size = 0
+
+            for a in nbhd2:
+                if a !=nclusters:
+                    cover2[a]+=1
+                    nbhd2_size+=1
+
+            if nbhd2_size/windows_pixels < .75:
+                j+=1
+                continue
+
+            numerator = 0
+            for c in range(nclusters):
+                numerator += np.abs(cover1[c]-cover2[c])
+
+            bray_curtis[i,j] = numerator/(nbhd1_size+nbhd2_size)
+            bray_curtis[j,i] = bray_curtis[i,j]
+            j+=1
+        i+=1
+    return bray_curtis
+
+@jit(nopython=True)
+def calc_alpha(classes,window,nclusters):
+    shannon = np.zeros(classes.shape)
+    simpson = np.zeros(classes.shape)
+    windows_pixels = window**2
+
+    lines = classes.shape[0]
+    columns = classes.shape[0]
+
+    half_window = int(window/2)
+
+    for line in range(half_window,lines-half_window):
+        for col in range(half_window,columns-half_window):
+            nbhd = classes[line-half_window:line+half_window,col-half_window:col+half_window].flatten()
+            cover = [0 for x in range(nclusters)]
+            nbhd_size = 0
+
+            for a in nbhd:
+                if a !=nclusters:
+                    cover[a]+=1
+                    nbhd_size+=1
+
+            if nbhd_size/windows_pixels < .75:
+                continue
+
+            shn,smp = 0,0
+
+            for c in range(nclusters):
+                if cover[c] !=0:
+                    p = cover[c]/nbhd_size
+                    shn += p * np.log(p)
+                    smp += p**2
+            shannon[line,col] = -shn
+            simpson[line,col] = 1/smp
+    return shannon,simpson
+
 def main():
     '''
     Spectral diversity metrics
 
-    Adapted from https://github.com/jbferet/biodivMapR
+    Adapted from:
+
+        Féret, J. B., & de Boissieu, F. (2020).
+        biodivMapR: An r package for α‐and β‐diversity mapping
+        using remotely sensed images.
+        Methods in Ecology and Evolution, 11(1), 64-70.
+        https://doi.org/10.1111/2041-210X.13310
 
     '''
 
@@ -53,7 +154,7 @@ def main():
     parser.add_argument('out_dir', type=str,
                         help='Output directory')
     parser.add_argument('--verbose', action='store_true')
-    parser.add_argument('--clusters', type=int, default=25,
+    parser.add_argument('--nclusters', type=int, default=25,
                         help='Export PCA image')
     parser.add_argument('--window', type=int, default=5,
                         help='Window size for calculating diversity metrics')
@@ -61,14 +162,15 @@ def main():
                         help='Export PCA image')
     parser.add_argument('--species', action='store_true',
                         help='Export spectral species map')
+    parser.add_argument('--ncpus', type=int, default=1,
+                        help='Number of CPUs for MDS')
 
     args = parser.parse_args()
 
     rfl = htl.HyTools()
     rfl.read_file(args.rfl_file,'envi')
 
-    ndvi_thres = .3
-    rfl.mask['sample'] = rfl.ndi() > ndvi_thres
+    rfl.mask['sample'] = rfl.mask['no_data']
 
     # Sample data
     X  = subsample(rfl,.1)
@@ -81,7 +183,7 @@ def main():
     X = X[~np.isnan(X.sum(axis=1)) & ~np.isinf(X.sum(axis=1)),:]
 
     #Perform initial PCA fite
-    pca = PCA(n_components=20)
+    pca = PCA(n_components=40)
     pca.fit(X)
 
     #Refit
@@ -120,10 +222,10 @@ def main():
 
     #Cluster PCA data
     pca_sample= pca.transform(X.astype(float))
-    clusters = KMeans(n_clusters=25)
+    clusters = KMeans(n_clusters=args.nclusters)
     clusters.fit(pca_sample)
     classes = clusters.predict(pca_transform.reshape(rfl.lines*rfl.columns,comps)).reshape(rfl.lines,rfl.columns)
-    classes[rfl.ndi() < ndvi_thres] = rfl.no_data
+    classes[~rfl.mask['no_data']] = args.nclusters
 
     if args.species:
         header = rfl.get_header()
@@ -135,25 +237,14 @@ def main():
         writer = WriteENVI(out_file,header)
         writer.write_band(classes,0)
 
-    shannon = np.zeros((rfl.lines,rfl.columns))
-    simpson = np.zeros((rfl.lines,rfl.columns))
+    if args.verbose:
+        print("Calculating alpha diversity metrics.....")
 
-    for line in range(args.window,rfl.lines-args.window):
-        for col in range(args.window,rfl.columns-args.window):
-            nbhd = classes[line-args.window:line+args.window,col-args.window:col+args.window]
-            nbhd = nbhd[nbhd!=rfl.no_data]
+    shannon,simpson = calc_alpha(classes,args.window,args.nclusters)
+    shannon[~rfl.mask['no_data']] = -9999
+    simpson[~rfl.mask['no_data']] = -9999
 
-            nbhd_sum= np.array(list(Counter(nbhd.flatten()).values()))
-
-            sh = -np.sum(nbhd_sum/nbhd_sum.sum() * np.log(nbhd_sum/nbhd_sum.sum()))
-            sim = 1- ((nbhd_sum*(nbhd_sum-1)).sum())/(nbhd_sum.sum()*(nbhd_sum.sum()-1))
-
-            shannon[line,col] = sh
-            simpson[line,col] = sim
-        if args.verbose:
-            progbar(line,rfl.lines, full_progbar = 100)
-
-    #Export diversity indices
+    #Export alpha diversity indices
     out_file = args.out_dir + rfl.base_name + '_alpha_diversity'
     header = rfl.get_header()
     header['bands']= 2
@@ -162,22 +253,51 @@ def main():
     writer.write_band(shannon[:,:],0)
     writer.write_band(simpson[:,:],1)
 
+    #Run beta diversity questions
+    column_end =args.window*(rfl.columns//args.window)
+    line_end = args.window*(rfl.lines//args.window)
 
-def subsample(hy_obj,sample_size):
+    image_blocks = view_as_blocks(classes[:line_end,:column_end],(args.window,args.window))
+    image_blocks = image_blocks.reshape(image_blocks.shape[0]*image_blocks.shape[1],
+                                        args.window,args.window)
+    if args.verbose:
+        print("Calculating dissimilarity matrix.......")
 
-    sub_samples = np.zeros((hy_obj.lines,hy_obj.columns)).astype(bool)
-    idx = np.array(np.where(hy_obj.mask['sample'])).T
-    idxRand= idx[np.random.choice(range(len(idx)),int(len(idx)*sample_size), replace = False)].T
-    sub_samples[idxRand[0],idxRand[1]] = True
-    hy_obj.mask['samples'] = sub_samples
+    bray_curtis = calc_bray_curtis_blocks(image_blocks,classes,args.nclusters)
 
-    X = []
+    #Filter dissimilarity matrix
+    bc_filt = bray_curtis[:,bray_curtis.sum(axis=0) != bray_curtis.shape[1]*-4]
+    bc_filt = bc_filt[bc_filt.sum(axis=1) != bc_filt.shape[1]*-4,:]
 
-    hy_obj.create_bad_bands([[300,400],[1300,1450],[1780,2000],[2450,2600]])
-    for band_num,band in enumerate(hy_obj.bad_bands):
-        if ~band:
-            X.append(hy_obj.get_band(band_num,mask='samples'))
-    return  np.array(X).T
+    if args.verbose:
+        print("Performing multidimensional scaling.....")
+
+    nmds = MDS(n_components=3,n_jobs=args.ncpus)
+    nmds.dissimilarity = 'precomputed'
+    coords = nmds.fit_transform(bc_filt)
+
+    full_coords = np.zeros((bray_curtis.shape[0],3))
+    full_coords[bray_curtis.sum(axis=0) != bray_curtis.shape[0]*-4] = coords
+
+    image_blocks = view_as_blocks(classes[:1150,:1150],(args.window,args.window))
+    beta = full_coords.reshape((image_blocks.shape[0],image_blocks.shape[1],3))
+    beta[image_blocks.mean(axis = (2,3))<0] = np.nan
+
+    header = rfl.get_header()
+    header['bands']= 3
+    header['band names']= ['species']
+    header['wavelength']= []
+    header['lines']= beta.shape[0]
+    header['samples']= beta.shape[0]
+    pixel_x,pixel_y =header['map info'][5:7]
+    header['map info'][5:7] = [str(float(pixel_x)*args.window),
+                               str(float(pixel_y)*args.window)]
+    header['data ignore value'] = 0
+
+    out_file = args.out_dir + rfl.base_name + '_beta_diversity'
+    writer = WriteENVI(out_file,header)
+    for band in range(3):
+        writer.write_band(beta[:,:,band],band)
 
 if __name__== "__main__":
     main()
